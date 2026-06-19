be3c5c847895d649e3cb8cb9558643ea3f0017a3
RyanXu02 <ryanxu20021223@gmail.com>
2026-06-19 15:36:05 -0400

    glad, imgui, jolt, spdlog, pch stuff, main tests

---
+++ SpeedEngine/ThirdParty/pch.h
@@ -0,0 +1,34 @@
+// Do not add files here that will be updated frequently 
+// as this negates the performance advantage of precompile header.
+#pragma once
+
+#include <stdio.h>
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <atomic>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <shared_mutex>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+#include <Jolt/Jolt.h>
---
+++ SpeedEngine/src/main.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+
+// using namespace Speed;
+
+#include "glm.hpp"
+#include "glad/gl.h"
+#include "spdlog.h"
+#include "imgui.h"
+#include "fmod.hpp"
+
+// Jolt includes
+#include <Jolt/RegisterTypes.h>
+#include <Jolt/Core/Factory.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Physics/PhysicsSettings.h>
+#include <Jolt/Physics/PhysicsSystem.h>
+#include <Jolt/Physics/Collision/Shape/BoxShape.h>
+#include <Jolt/Physics/Collision/Shape/SphereShape.h>
+#include <Jolt/Physics/Body/BodyCreationSettings.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+
+
+int main() {
+    
+    std::queue<int> myQueue;
+	myQueue.push(1);
+	printf("Hello world! %d\n", myQueue.front());
+
+	glm::vec3 test = glm::vec3(1.0f, 2.0f, 3.0f);
+	printf("Hello world! %f\n", test.x);
+
+    JPH::RegisterDefaultAllocator();
+
+    spdlog::info("test");
+
+    //SN::AppConfig appConfig{};
+    //appConfig.title = "Slippery Slope";
+    //appConfig.windowPlatform = SN::WindowPlatform::GLFW;
+    //appConfig.renderPlatform = SN::RenderPlatform::OPENGL;
+    //appConfig.guiPlatform = SN::GuiPlatform::IMGUI;
+    //appConfig.os = SN::OperatingSystem::WINDOW;
+    //appConfig.audioPlatform = SN::AudioPlatform::FMOD;
+    //appConfig.width = 1920;
+    //appConfig.height = 1080;
+    //appConfig.vsync = false;    // set to true to fix screen tearing but currently breaks physics
+    //appConfig.developmentMode = false;
+    //appConfig.updateFPS = 120.0;
+    //appConfig.renderFPS = 120.0;
+
+
+    //SN::Engine engine(appConfig);
+    //engine.init();
+    //engine.start();
+    //engine.run();
+    //engine.shutDown();
+
+    return 0;
+}
