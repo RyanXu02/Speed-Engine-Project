dda9559473d70da1f3695c81c05cd57c4438a6c1
RyanXu02 <ryanxu20021223@gmail.com>
2026-06-22 13:59:56 -0400

    Core directory, engine class

---
+++ SpeedEngine/src/Core/Engine.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "Engine.h"
+
+namespace SE
+{
+	Engine::Engine()
+		: m_isRunning(false), m_deltaTime(0.0)
+	{
+	}
+	Engine::~Engine()
+	{
+	}
+	void Engine::init()
+	{
+		
+	}
+	void Engine::start()
+	{
+		m_isRunning = true;
+	}
+	void Engine::run()
+	{
+		while (m_isRunning)
+		{
+			// Update delta time
+			m_deltaTime = 0.016; // Placeholder for actual delta time calculation
+			// Handle input
+			// Update game logic
+			// Render
+		}
+	}
+	void Engine::stop()
+	{
+		m_isRunning = false;
+	}
+}
---
+++ SpeedEngine/src/Core/Engine.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace SE
+{
+	class Engine
+	{
+	public:
+		Engine();
+		~Engine();
+
+		void init();
+		void start();
+		void run();
+		void stop();
+
+	private:
+		bool m_isRunning;
+		double m_deltaTime;
+	};
+}
