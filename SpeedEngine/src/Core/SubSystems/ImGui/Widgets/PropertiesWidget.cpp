cbeb83da124749916de8075ea3499bb27d9325e2
logenlol <weegeehead@gmail.com>
2026-08-06 13:48:33 -0700

    start of properties widget
    also bug in scene preview where resizing widget stretches image

---
+++ SpeedEngine/src/Core/SubSystems/ImGui/Widgets/PropertiesWidget.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "PropertiesWidget.h"
+
+namespace SE {
+
+}
---
+++ SpeedEngine/src/Core/SubSystems/ImGui/Widgets/PropertiesWidget.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../Widget.h"
+
+namespace SE {
+    class PropertiesWidget : public Widget
+    {
+    public:
+        PropertiesWidget();
+
+        void render();
+        void shutdown();
+    private:
+
+    };
+
+}
+
